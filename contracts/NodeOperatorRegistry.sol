fdaac69c98c2f566835e84983fd6907a8f97e22f
idirall22 <idirall22@gmail.com>
2022-02-17 10:34:35 +0100

    added initialize function, role and simple storage

--- contracts/NodeOperatorRegistry.sol
+++ contracts/NodeOperatorRegistry.sol
@@ -19,6 +19,38 @@ contract NodeOperatorRegistry is
     AccessControlUpgradeable,
     ReentrancyGuardUpgradeable
 {
+    /// @notice stakeManager interface.
+    IStakeManager public stakeManager;
+
+    /// @notice stMatic interface.
+    IStMATIC public stMATIC;
+
+    /// @notice all the roles.
+    bytes32 public constant DAO_ROLE = keccak256("LIDO_DAO");
+
+    /// @notice Check if the msg.sender has permission.
+    /// @param _role role needed to call function.
+    modifier userHasRole(bytes32 _role) {
+        require(hasRole(_role, msg.sender), "unauthorized");
+        _;
+    }
+
+    /// @notice Initialize the NodeOperatorRegistry contract.
+    function initialize(IStakeManager _stakeManager, IStMATIC _stMATIC)
+        external
+        initializer
+    {
+        __Pausable_init();
+        __AccessControl_init();
+        __ReentrancyGuard_init();
+
+        stakeManager = _stakeManager;
+        stMATIC = _stMATIC;
+
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(DAO_ROLE, msg.sender);
+    }
+
     /// @notice Add a new node operator registry to the system.
     /// ONLY DAO can execute this function.
     /// @param _validatorId the validator id on stakeManager.
@@ -26,7 +58,7 @@ contract NodeOperatorRegistry is
     function addNodeOperatorRegistry(
         uint256 _validatorId,
         address _rewardAddress
-    ) external override {}
+    ) external override userHasRole(DAO_ROLE) {}
 
     /// @notice Remove a new node operator registry from the system and
     /// ONLY DAO can execute this function.
@@ -35,17 +67,25 @@ contract NodeOperatorRegistry is
     function removeNodeOperatorRegistry(uint256 _validatorId)
         external
         override
+        userHasRole(DAO_ROLE)
     {}
 
     /// @notice Set StMatic address.
     /// ONLY DAO can call this function
     /// @param _newStMatic new stMatic address.
-    function setStMaticAddress(address _newStMatic) external override {}
+    function setStMaticAddress(address _newStMatic)
+        external
+        override
+        userHasRole(DAO_ROLE)
+    {}
 
     /// @notice Update the reward address of a Node Operator Registry.
     /// ONLY Operator owner can call this function
     /// @param _newRewardAddress the new reward address.
-    function setRewardAddress(address _newRewardAddress) external override {}
+    function setRewardAddress(address _newRewardAddress)
+        external
+        override
+    {}
 
     /// @notice List all node operator registry available in the system.
     /// @return Returns a list of Active node operator registry.
